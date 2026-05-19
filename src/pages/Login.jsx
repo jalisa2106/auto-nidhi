0fe611aaf39dd596c1e0b908c88d03770dd6e6e1
Dhruvil <dhruvilgandhi1203@gmail.com>
2026-05-19 17:55:35 +0530

    intiating the structure

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Form, Input, Button, Card, message } from 'antd'
+const Login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
