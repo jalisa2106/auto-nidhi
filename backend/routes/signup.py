57a00bf795a8c9724fb313fc60eb501abeb249bb
Dhruvil <dhruvilgandhi1203@gmail.com>
2026-05-20 15:51:09 +0530

    made some changes in backend

--- backend/routes/signup.py
+++ backend/routes/signup.py
@@ -2,6 +2,11 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 router = APIRouter()
+ROLE_PASSKEYS = {
+    "admin": "AUTONIDHI-ADMIN-001",
+    "accountant": "AUTONIDHI-ACC-001",
+    "data_entry": "AUTONIDHI-DE-001",
+}
 
 # ================= Dummy Database =================
 
@@ -26,6 +31,12 @@ def signup(data: SignupData):
         return {
             "error": "Passwords do not match"
         }
+    if data.role in ROLE_PASSKEYS:
+        if(data.passkey != ROLE_PASSKEYS[data.role]):
+            return {
+                "error": "Inavalid Passkey for this role"
+            }
+
 
     # Existing user check
     for user in users:
